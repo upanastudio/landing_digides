58370c79effbac2db06c6091ebfe9520e1f6387e
guntekhunter <72636466+guntekhunter@users.noreply.github.com>
2021-01-19 11:46:19 +0800

    hover card, ganti jawaban umum sesuai kategori

---
+++ assets/js/faq-script.js
@@ -0,0 +1,216 @@
+function muncul1() {
+    aktif1 = document.querySelector('.pertanyaan1');
+    aktif2 = document.querySelector('.pertanyaan2');
+    aktif3 = document.querySelector('.pertanyaan3');
+    aktif4 = document.querySelector('.pertanyaan4');
+    aktif5 = document.querySelector('.pertanyaan5');
+    aktif6 = document.querySelector('.pertanyaan6');
+    aktif1.classList.add('aktif');
+    aktif2.classList.remove('aktif');
+    aktif3.classList.remove('aktif');
+    aktif4.classList.remove('aktif');
+    aktif5.classList.remove('aktif');
+    aktif6.classList.remove('aktif');
+    document.getElementById("div1").style.display = "block";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.querySelector('.logo1').src = "assets/img/iconCard/city-hall-hover.svg";
+    document.querySelector('.logo2').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo3').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo4').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo5').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo6').src = "assets/img/iconCard/city-hall.svg";
+}
+function muncul2() {
+    aktif1 = document.querySelector('.pertanyaan1');
+    aktif2 = document.querySelector('.pertanyaan2');
+    aktif3 = document.querySelector('.pertanyaan3');
+    aktif4 = document.querySelector('.pertanyaan4');
+    aktif5 = document.querySelector('.pertanyaan5');
+    aktif6 = document.querySelector('.pertanyaan6');
+    aktif1.classList.remove('aktif');
+    aktif2.classList.add('aktif');
+    aktif3.classList.remove('aktif');
+    aktif4.classList.remove('aktif');
+    aktif5.classList.remove('aktif');
+    aktif6.classList.remove('aktif');
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "block";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.querySelector('.logo1').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo2').src = "assets/img/iconCard/city-hall-hover.svg";
+    document.querySelector('.logo3').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo4').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo5').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo6').src = "assets/img/iconCard/city-hall.svg";
+}
+function muncul3() {
+    aktif1 = document.querySelector('.pertanyaan1');
+    aktif2 = document.querySelector('.pertanyaan2');
+    aktif3 = document.querySelector('.pertanyaan3');
+    aktif4 = document.querySelector('.pertanyaan4');
+    aktif5 = document.querySelector('.pertanyaan5');
+    aktif6 = document.querySelector('.pertanyaan6');
+    aktif1.classList.remove('aktif');
+    aktif2.classList.remove('aktif');
+    aktif3.classList.add('aktif');
+    aktif4.classList.remove('aktif');
+    aktif5.classList.remove('aktif');
+    aktif6.classList.remove('aktif');
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "block";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.querySelector('.logo1').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo2').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo3').src = "assets/img/iconCard/city-hall-hover.svg";
+    document.querySelector('.logo4').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo5').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo6').src = "assets/img/iconCard/city-hall.svg";
+}
+function muncul4() {
+    aktif1 = document.querySelector('.pertanyaan1');
+    aktif2 = document.querySelector('.pertanyaan2');
+    aktif3 = document.querySelector('.pertanyaan3');
+    aktif4 = document.querySelector('.pertanyaan4');
+    aktif5 = document.querySelector('.pertanyaan5');
+    aktif6 = document.querySelector('.pertanyaan6');
+    aktif1.classList.remove('aktif');
+    aktif2.classList.remove('aktif');
+    aktif3.classList.remove('aktif');
+    aktif4.classList.add('aktif');
+    aktif5.classList.remove('aktif');
+    aktif6.classList.remove('aktif');
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "block";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "none";
+    document.querySelector('.logo1').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo2').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo3').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo4').src = "assets/img/iconCard/city-hall-hover.svg";
+    document.querySelector('.logo5').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo6').src = "assets/img/iconCard/city-hall.svg";
+}
+function muncul5() {
+    aktif1 = document.querySelector('.pertanyaan1');
+    aktif2 = document.querySelector('.pertanyaan2');
+    aktif3 = document.querySelector('.pertanyaan3');
+    aktif4 = document.querySelector('.pertanyaan4');
+    aktif5 = document.querySelector('.pertanyaan5');
+    aktif6 = document.querySelector('.pertanyaan6');
+    aktif1.classList.remove('aktif');
+    aktif2.classList.remove('aktif');
+    aktif3.classList.remove('aktif');
+    aktif4.classList.remove('aktif');
+    aktif5.classList.add('aktif');
+    aktif6.classList.remove('aktif');
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "block";
+    document.getElementById("div6").style.display = "none";
+    document.querySelector('.logo1').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo2').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo3').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo4').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo5').src = "assets/img/iconCard/city-hall-hover.svg";
+    document.querySelector('.logo6').src = "assets/img/iconCard/city-hall.svg";
+}
+function muncul6() {
+    aktif1 = document.querySelector('.pertanyaan1');
+    aktif2 = document.querySelector('.pertanyaan2');
+    aktif3 = document.querySelector('.pertanyaan3');
+    aktif4 = document.querySelector('.pertanyaan4');
+    aktif5 = document.querySelector('.pertanyaan5');
+    aktif6 = document.querySelector('.pertanyaan6');
+    aktif1.classList.remove('aktif');
+    aktif2.classList.remove('aktif');
+    aktif3.classList.remove('aktif');
+    aktif4.classList.remove('aktif');
+    aktif5.classList.remove('aktif');
+    aktif6.classList.add('aktif');
+    document.getElementById("div1").style.display = "none";
+    document.getElementById("div2").style.display = "none";
+    document.getElementById("div3").style.display = "none";
+    document.getElementById("div4").style.display = "none";
+    document.getElementById("div5").style.display = "none";
+    document.getElementById("div6").style.display = "block";
+    document.querySelector('.logo1').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo2').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo3').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo4').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo5').src = "assets/img/iconCard/city-hall.svg";
+    document.querySelector('.logo6').src = "assets/img/iconCard/city-hall-hover.svg";
+}
+$('.aktif1').on('mouseenter', function () {
+    $('.pertanyaan1').addClass('card-hover');
+});
+$('.aktif1').on('mouseleave', function () {
+    $('.pertanyaan1').removeClass('card-hover');
+});
+$('.aktif1').on('click', function () {
+    $('.pertanyaan1').removeClass('card-hover');
+    $('.pertanyaan1').off('mouseenter mouseleave');
+});
+$('.aktif2').on('mouseenter', function () {
+    $('.pertanyaan2').addClass('card-hover');
+});
+$('.aktif2').on('mouseleave', function () {
+    $('.pertanyaan2').removeClass('card-hover');
+});
+$('.aktif2').on('click', function () {
+    $('.pertanyaan2').removeClass('card-hover');
+    $('.pertanyaan2').off('mouseenter mouseleave');
+});
+$('.aktif3').on('mouseenter', function () {
+    $('.pertanyaan3').addClass('card-hover');
+});
+$('.aktif3').on('mouseleave', function () {
+    $('.pertanyaan3').removeClass('card-hover');
+});
+$('.aktif3').on('click', function () {
+    $('.pertanyaan3').removeClass('card-hover');
+    $('.pertanyaan3').off('mouseenter mouseleave');
+});
+$('.aktif4').on('mouseenter', function () {
+    $('.pertanyaan4').addClass('card-hover');
+});
+$('.aktif4').on('mouseleave', function () {
+    $('.pertanyaan4').removeClass('card-hover');
+});
+$('.aktif4').on('click', function () {
+    $('.pertanyaan4').removeClass('card-hover');
+    $('.pertanyaan4').off('mouseenter mouseleave');
+});
+$('.aktif5').on('mouseenter', function () {
+    $('.pertanyaan5').addClass('card-hover');
+});
+$('.aktif5').on('mouseleave', function () {
+    $('.pertanyaan5').removeClass('card-hover');
+});
+$('.aktif5').on('click', function () {
+    $('.pertanyaan5').removeClass('card-hover');
+    $('.pertanyaan5').off('mouseenter mouseleave');
+});
+$('.aktif6').on('mouseenter', function () {
+    $('.pertanyaan6').addClass('card-hover');
+});
+$('.aktif6').on('mouseleave', function () {
+    $('.pertanyaan6').removeClass('card-hover');
+});
+$('.aktif6').on('click', function () {
+    $('.pertanyaan6').removeClass('card-hover');
+    $('.pertanyaan6').off('mouseenter mouseleave');
+});
