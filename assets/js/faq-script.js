1070739aad72b1802a8d5394ef87cc58e69a92d3
amrulamin <amrulamin23@gmail.com>
2021-02-15 13:58:35 +0800

    Tambahan Konten dan assets gambar

--- assets/js/faq-script.js
+++ assets/js/faq-script.js
@@ -1,30 +1,13 @@
 // function menampilkan kategori
-for (var i; i < 7; i++) {
-    function muncul1() {
-        for (var i = 1; i < 7; i++) {
-            aktif = document.querySelector('.pertanyaan' + i);
-            if (i !== 1) {
-                aktif.classList.remove('aktif');
-            } else {
-                aktif.classList.add('aktif');
-            }
-        }
-        for (var i = 1; i < 7; i++) {
-            if (i !== 1) {
-                document.getElementById('div' + i).style.display = "none";
-            } else {
-                document.getElementById('div' + i).style.display = "block";
-            }
-        }
-        for (var i = 1; i < 7; i++) {
-            if (i !== 1) {
-                document.querySelector('.logo' + i).src = "assets/img/iconCard/city-hall.svg";
-            } else {
-                document.querySelector('.logo' + i).src = "assets/img/iconCard/city-hall-hover.svg";
-            }
-        }
+
+for (var i = 1; i <= 7; i++) {
+    if (i !== 7) {
+        document.getElementById('div' + i).style.display = "none";
+    } else {
+        document.getElementById('div' + i).style.display = "block";
     }
 }
+
 function muncul1() {
     for (var i = 1; i < 7; i++) {
         aktif = document.querySelector('.pertanyaan' + i);
@@ -34,7 +17,7 @@ function muncul1() {
             aktif.classList.add('aktif');
         }
     }
-    for (var i = 1; i < 7; i++) {
+    for (var i = 1; i < 8; i++) {
         if (i !== 1) {
             document.getElementById('div' + i).style.display = "none";
         } else {
@@ -49,6 +32,7 @@ function muncul1() {
         }
     }
 }
+
 function muncul2() {
     for (var i = 1; i < 7; i++) {
         aktif = document.querySelector('.pertanyaan' + i);
@@ -58,7 +42,7 @@ function muncul2() {
             aktif.classList.add('aktif');
         }
     }
-    for (var i = 1; i < 7; i++) {
+    for (var i = 1; i < 8; i++) {
         if (i !== 2) {
             document.getElementById('div' + i).style.display = "none";
         } else {
@@ -73,6 +57,7 @@ function muncul2() {
         }
     }
 }
+
 function muncul3() {
     for (var i = 1; i < 7; i++) {
         aktif = document.querySelector('.pertanyaan' + i);
@@ -82,7 +67,7 @@ function muncul3() {
             aktif.classList.add('aktif');
         }
     }
-    for (var i = 1; i < 7; i++) {
+    for (var i = 1; i < 8; i++) {
         if (i !== 3) {
             document.getElementById('div' + i).style.display = "none";
         } else {
@@ -97,6 +82,7 @@ function muncul3() {
         }
     }
 }
+
 function muncul4() {
     for (var i = 1; i < 7; i++) {
         aktif = document.querySelector('.pertanyaan' + i);
@@ -106,7 +92,7 @@ function muncul4() {
             aktif.classList.add('aktif');
         }
     }
-    for (var i = 1; i < 7; i++) {
+    for (var i = 1; i < 8; i++) {
         if (i !== 4) {
             document.getElementById('div' + i).style.display = "none";
         } else {
@@ -121,6 +107,7 @@ function muncul4() {
         }
     }
 }
+
 function muncul5() {
     for (var i = 1; i < 7; i++) {
         aktif = document.querySelector('.pertanyaan' + i);
@@ -130,7 +117,7 @@ function muncul5() {
             aktif.classList.add('aktif');
         }
     }
-    for (var i = 1; i < 7; i++) {
+    for (var i = 1; i < 8; i++) {
         if (i !== 5) {
             document.getElementById('div' + i).style.display = "none";
         } else {
@@ -145,6 +132,7 @@ function muncul5() {
         }
     }
 }
+
 function muncul6() {
     for (var i = 1; i < 7; i++) {
         aktif = document.querySelector('.pertanyaan' + i);
@@ -154,7 +142,7 @@ function muncul6() {
             aktif.classList.add('aktif');
         }
     }
-    for (var i = 1; i < 7; i++) {
+    for (var i = 1; i < 8; i++) {
         if (i !== 6) {
             document.getElementById('div' + i).style.display = "none";
         } else {
@@ -171,63 +159,63 @@ function muncul6() {
 }
 // merubaha warna pertanyaan umum
 
-$('.aktif1').on('mouseenter', function () {
+$('.aktif1').on('mouseenter', function() {
     $('.pertanyaan1').addClass('card-hover');
 });
-$('.aktif1').on('mouseleave', function () {
+$('.aktif1').on('mouseleave', function() {
     $('.pertanyaan1').removeClass('card-hover');
 });
-$('.aktif1').on('click', function () {
+$('.aktif1').on('click', function() {
     $('.pertanyaan1').removeClass('card-hover');
     $('.pertanyaan1').off('mouseenter mouseleave');
 });
-$('.aktif2').on('mouseenter', function () {
+$('.aktif2').on('mouseenter', function() {
     $('.pertanyaan2').addClass('card-hover');
 });
-$('.aktif2').on('mouseleave', function () {
+$('.aktif2').on('mouseleave', function() {
     $('.pertanyaan2').removeClass('card-hover');
 });
-$('.aktif2').on('click', function () {
+$('.aktif2').on('click', function() {
     $('.pertanyaan2').removeClass('card-hover');
     $('.pertanyaan2').off('mouseenter mouseleave');
 });
-$('.aktif3').on('mouseenter', function () {
+$('.aktif3').on('mouseenter', function() {
     $('.pertanyaan3').addClass('card-hover');
 });
-$('.aktif3').on('mouseleave', function () {
+$('.aktif3').on('mouseleave', function() {
     $('.pertanyaan3').removeClass('card-hover');
 });
-$('.aktif3').on('click', function () {
+$('.aktif3').on('click', function() {
     $('.pertanyaan3').removeClass('card-hover');
     $('.pertanyaan3').off('mouseenter mouseleave');
 });
-$('.aktif4').on('mouseenter', function () {
+$('.aktif4').on('mouseenter', function() {
     $('.pertanyaan4').addClass('card-hover');
 });
-$('.aktif4').on('mouseleave', function () {
+$('.aktif4').on('mouseleave', function() {
     $('.pertanyaan4').removeClass('card-hover');
 });
-$('.aktif4').on('click', function () {
+$('.aktif4').on('click', function() {
     $('.pertanyaan4').removeClass('card-hover');
     $('.pertanyaan4').off('mouseenter mouseleave');
 });
-$('.aktif5').on('mouseenter', function () {
+$('.aktif5').on('mouseenter', function() {
     $('.pertanyaan5').addClass('card-hover');
 });
-$('.aktif5').on('mouseleave', function () {
+$('.aktif5').on('mouseleave', function() {
     $('.pertanyaan5').removeClass('card-hover');
 });
-$('.aktif5').on('click', function () {
+$('.aktif5').on('click', function() {
     $('.pertanyaan5').removeClass('card-hover');
     $('.pertanyaan5').off('mouseenter mouseleave');
 });
-$('.aktif6').on('mouseenter', function () {
+$('.aktif6').on('mouseenter', function() {
     $('.pertanyaan6').addClass('card-hover');
 });
-$('.aktif6').on('mouseleave', function () {
+$('.aktif6').on('mouseleave', function() {
     $('.pertanyaan6').removeClass('card-hover');
 });
-$('.aktif6').on('click', function () {
+$('.aktif6').on('click', function() {
     $('.pertanyaan6').removeClass('card-hover');
     $('.pertanyaan6').off('mouseenter mouseleave');
 });
